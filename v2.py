5fe6e7fca68463507b99345042b33a46d501422c
Niels Koster <niels.koster@portavita.nl>
2019-03-21 13:31:35 +0100

    Better python.

--- v2.py
+++ v2.py
@@ -1,4 +1,5 @@
-import cv2, sys
+import cv2
+import sys
 
 arg = 0
 if len(sys.argv) > 1:
@@ -14,15 +15,15 @@ scale = 1000.0 / width
 
 while check:
     resized = cv2.resize(frame, ((int(width * scale)),
-        (int(height * scale))), fx = scale, fy = scale,
-        interpolation = cv2.INTER_LINEAR)
+        (int(height * scale))), fx=scale, fy=scale,
+        interpolation=cv2.INTER_LINEAR)
     gray_img = cv2.cvtColor(resized, cv2.COLOR_BGR2GRAY)
-    faces = face_cascade.detectMultiScale(gray_img, scaleFactor = 1.2, minNeighbors = 5)
+    faces = face_cascade.detectMultiScale(gray_img, scaleFactor=1.2, minNeighbors=5)
     for fx, fy, fw, fh in faces:
         cv2.rectangle(resized, (fx, fy), (fx + fw, fy + fh), (0, 0, 0), 3)
-        roi_gray = gray_img[fy : fy + fh, fx : fx + fw]
-        roi_color = resized[fy : fy + fh, fx : fx + fw]
-        eyes = eye_cascade.detectMultiScale(roi_gray, scaleFactor = 1.25, minNeighbors = 5)
+        roi_gray = gray_img[fy: fy + fh, fx: fx + fw]
+        roi_color = resized[fy: fy + fh, fx: fx + fw]
+        eyes = eye_cascade.detectMultiScale(roi_gray, scaleFactor=1.25, minNeighbors=5)
         for ex, ey, ew, eh in eyes:
             cv2.circle(roi_color, (ex + (ew / 2), ey + (eh / 2)), ew, (255, 255, 255), 3)
     cv2.imshow('video', resized)
@@ -33,4 +34,4 @@ while check:
 
 video.release()
 cv2.destroyAllWindows()
-print width, height, scale
+print(width, height, scale)

8394d57cc5579f1af902aab782f2fda582d7ec1b
Niels Koster <n.koster@portavita.eu>
2019-04-04 22:59:26 +0200

    green

--- t2.py
+++ t2.py
@@ -5,7 +5,7 @@ arg = 0
 if len(sys.argv) > 1:
     arg = sys.argv[1]
 
-face_cascade = cv2.CascadeClassifier('./haar/haarcascade_frontalface_alt2.xml')
+face_cascade = cv2.CascadeClassifier('./haar/haarcascade_frontalface_default.xml')
 eye_cascade = cv2.CascadeClassifier('./haar/haarcascade_eye.xml')
 
 img = cv2.imread(arg, 1)
@@ -25,7 +25,7 @@ gray_img = cv2.cvtColor(resized, cv2.COLOR_BGR2GRAY)
 faces = face_cascade.detectMultiScale(gray_img, scaleFactor = 1.2, minNeighbors = 5)
 
 for fx, fy, fw, fh in faces:
-    cv2.rectangle(resized, (fx, fy), (fx + fw, fy + fh), (0, 0, 0), 3)
+    cv2.rectangle(resized, (fx, fy), (fx + fw, fy + fh), (0, 255, 0), 3)
     roi_gray = gray_img[fy: fy + fh, fx: fx + fw]
     roi_color = resized[fy: fy + fh, fx: fx + fw]
     eyes = eye_cascade.detectMultiScale(roi_gray, scaleFactor=1.25, minNeighbors=5)
@@ -33,6 +33,7 @@ for fx, fy, fw, fh in faces:
         cv2.circle(roi_color, (ex + (ew / 2), ey + (eh / 2)), ew, (255, 255, 255), 3)
     cv2.imwrite('./roi.png', roi_color)
 
-cv2.imshow('Faces and Eyes', resized)
-cv2.waitKey(0)
+cv2.imshow(arg, resized)
+while cv2.waitKey(0) != ord('q'):
+    continue
 cv2.destroyAllWindows()

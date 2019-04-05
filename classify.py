c4ca625f12b226559ead8e6379c79ca2f434f1b1
Niels Koster <n.koster@portavita.eu>
2019-04-05 11:03:01 +0200

    Do not check for faces during classification.

--- classify.py
+++ classify.py
@@ -14,8 +14,6 @@ else:
     print('No image provided.')
     exit(1)
 
-face_cascade = cv2.CascadeClassifier('./haar/haarcascade_frontalface_default.xml')
-
 img = cv2.imread(arg, 1)
 width, height = img.shape[1], img.shape[0]
 
@@ -28,27 +26,35 @@ resized = cv2.resize(img, ((int(width * scale)),
     (int(height * scale))), fx=scale, fy=scale,
     interpolation=cv2.INTER_LINEAR)
 
-gray_img = cv2.cvtColor(resized, cv2.COLOR_BGR2GRAY)
-
-faces = face_cascade.detectMultiScale(gray_img, scaleFactor = 1.2, minNeighbors = 5)
-
 def classify(arg, c):
     print(arg, c)
 
-if len(faces) > 0:
-    for fx, fy, fw, fh in faces:
-        cv2.rectangle(resized, (fx, fy), (fx + fw, fy + fh), (0, 255, 0), 3)
-        roi_gray = gray_img[fy: fy + fh, fx: fx + fw]
-        roi_color = resized[fy: fy + fh, fx: fx + fw]
-    cv2.imshow(arg, resized)
-    person = 'unknown'
-    key = cv2.waitKey(0)
-    if key == ord('l'):
-        person = 'laury'
-    if key == ord('n'):
-        person = 'niels'
-    cv2.destroyAllWindows()
-    if person != 'unknown':
-        path = './images/' + person + '/' + randomword(6) + '.png'
-        print(str(len(faces)) + ' ' + path)
-        cv2.imwrite(path , resized)
+for i in range(len(sys.argv)):
+    if i > 1:
+        print(sys.argv[i].split(':')[0] + ' ' + sys.argv[i].split(':')[1])
+
+cv2.imshow(arg, resized)
+person = 'unknown'
+key = cv2.waitKey(0)
+if key == ord('l'):
+    person = 'laury'
+if key == ord('n'):
+    person = 'niels'
+if key == ord('k'):
+    person = 'kees'
+if key == ord('m'):
+    person = 'max'
+if key == ord('d'):
+    person = 'danny'
+if key == ord('w'):
+    person = 'kriz'
+if key == ord('r'):
+    person = 'ray'
+
+cv2.destroyAllWindows()
+if person != 'unknown':
+    path = './images/' + person + '/' + randomword(6) + '.png'
+    print(path)
+    cv2.imwrite(path , resized)
+else:
+    print(arg)

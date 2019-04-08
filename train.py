bd22f27c8d681169b71c6518048992dff5ddd782
Niels Koster <n.koster@portavita.eu>
2019-04-08 23:52:43 +0200

    Recognizer and Labels.

--- train.py
+++ train.py
@@ -3,6 +3,7 @@ import os
 import sys
 import numpy as np
 from PIL import Image
+import pickle
 
 arg = ''
 if len(sys.argv) > 1:
@@ -13,7 +14,8 @@ else:
 
 face_cascade = cv2.CascadeClassifier('./haar/haarcascade_frontalface_alt2.xml')
 
-print(image_dir)
+recognizer = cv2.createLBPHFaceRecognizer()
+# print(image_dir)
 
 y_labels = []
 x_train = []
@@ -26,6 +28,11 @@ for root, dirs, files in os.walk(image_dir):
         if file.upper().endswith('PNG') or file.upper().endswith('JPG'):
             image = os.path.join(root, file)
             label = os.path.basename(root).replace(' ', '-').lower()
+            if not label in label_ids:
+                label_ids[label] = current_id
+                current_id += 1
+            id_ = label_ids[label]
+            #print(label_ids)
             pil_image = Image.open(image).convert('L') # Grayscale
             image_array = np.array(pil_image, 'uint8')
             width, height = image_array.shape[1], image_array.shape[0]
@@ -40,6 +47,15 @@ for root, dirs, files in os.walk(image_dir):
             roi = None
             for fx, fy, fw, fh in faces:
                 roi = resized[fy: fy + fh, fx: fx + fw]
-            print(label, image, width, height, scale, len(faces))
-            # y_labels.append(label)
+            # print(label, image, width, height, scale, len(faces))
+            y_labels.append(id_)
             x_train.append(roi)
+
+# print(y_labels)
+# print(x_train)
+
+with open('label.pickle', 'wb') as f:
+    pickle.dump(label_ids, f)
+
+recognizer.train(x_train, np.array(y_labels))
+recognizer.save('trainer.yml')
